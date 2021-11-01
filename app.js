5c1bf90f129a7cdc2cf0b1a8799ff8027debeb43
GLD5000 <garethlouisdevlin@gmail.com>
2021-11-01 21:31:50 +0000

    Slight Change

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const message = 'Hello World';
 console.log("This is my message: "+message);//add sign
 console.log('This is my "message": '+message);//add sign with single / double quotes
 console.log("This is my message:",message);//comma (adds a space)
-console.log(`This is my message: ${message}`);//backticks & ${}
+console.log(`This is my slightly different message: ${message}`);//backticks & ${}
